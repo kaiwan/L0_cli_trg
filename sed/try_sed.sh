0d55de4c6765e87d9b335382ab00f487cdd9f8c8
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2021-12-10 12:30:10 +0530

    minor cleanups

--- sed/try_sed.sh
+++ sed/try_sed.sh
@@ -2,19 +2,13 @@
 # Try sed
 # Ref:
 #  http://www.linuxjournal.com/article/7231?page=0,0
-
-source ./common.sh || {
- echo "common.sh ??"
- exit 1
-}
 TESTFILE=ny.txt
 
-ShowTitle "Double-space and then eliminate empty lines"
+echo "=== Double-space and then eliminate empty lines ==="
 # use sed to double-space the lines a file and then use sed to eliminate empty lines! :-)
 sed G ${TESTFILE}
 sed -e '/^$/d' ${TESTFILE}
 #sed -f dblspc.sed $TESTFILE |sed -e '/^$/d'
 
-ShowTitle "Remove the first word in every line"
+echo "=== Remove the first word in every line ==="
 cat $TESTFILE | sed -e 's/^ *[^ ]* //'
-

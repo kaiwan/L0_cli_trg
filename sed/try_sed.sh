86265bfbbe413cc00c329b0a9d2840619d3ad739
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-01-22 08:10:17 +0530

    Use bash strict mode

--- sed/try_sed.sh
+++ sed/try_sed.sh
@@ -2,6 +2,11 @@
 # Try sed
 # Ref:
 #  http://www.linuxjournal.com/article/7231?page=0,0
+
+# Set Bash unofficial 'strict mode'; _really_ helps catch bugs
+# ref: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+
 TESTFILE=ny.txt
 
 echo "=== Double-space and then eliminate empty lines ==="

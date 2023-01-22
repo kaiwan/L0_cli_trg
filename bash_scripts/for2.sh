e502e4300280468cccf93551eb54854f1c6f6bc1
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-01-22 07:52:36 +0530

    Better single char raw read

--- bash_scripts/for2.sh
+++ bash_scripts/for2.sh
@@ -6,23 +6,31 @@
 # the "done" statement.
 # 
 
+# Set Bash unofficial 'strict mode'; _really_ helps catch bugs
+# ref: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+
 FORFILE="forfile.output.txt"
 
-echo "echo before the for loop"
+echo "an echo before the for loop"
 
-for fil in $HOME/*; do
+for fil in ${HOME}/*; do
 	echo "Filename: $fil"
 	stat $fil
 	
-	echo "Press Return to continue, q to quit.."
-	read reply
-	if [[ "$reply" = "q" -o "$reply" = "Q" ]]; then
+	echo ">>> Press Return to continue, q to quit.. <<<"
+	# read:
+	#  -r: Raw read
+	#  -s: do not echo input character
+	#  -n 1: read only 1 character
+	read -r -s -n 1 reply
+	if [[ ${reply} = "" ]]; then
+		continue
+	elif [[ "$reply"=="q" || "$reply"=="Q" ]]; then
 		exit 0
 	fi
 done # > $FORFILE
 
 echo "echo after the for loop\
  exiting now.."
-
 exit 0
-
